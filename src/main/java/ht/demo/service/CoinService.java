c0dd9fcf1432a5fcfca7ee549e6f8cb2b3ae6b5f
htlord <s75757775@hotmail.com>
2025-02-15 21:18:21 +0800

    feat: impl create locale coin name and it

--- src/main/java/ht/demo/service/CoinService.java
+++ src/main/java/ht/demo/service/CoinService.java
@@ -1,7 +1,24 @@
 package ht.demo.service;
 
+import ht.demo.error.BadRequestWithErrorCodeException;
+import ht.demo.error.CoinCodedError;
+import ht.demo.repository.CoinRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CoinService {
+
+    private final CoinRepository coinRepository;
+
+    public void exist(String charName) throws BadRequestWithErrorCodeException {
+        coinRepository.findById(charName)
+            .orElseThrow(() ->
+                BadRequestWithErrorCodeException.builder()
+                    .codedError(CoinCodedError.CE_0001)
+                    .build()
+            );
+    }
+
 }
