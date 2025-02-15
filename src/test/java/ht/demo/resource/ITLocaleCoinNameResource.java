5e0e431c0506d26b68d059f31769eea201956396
htlord <s75757775@hotmail.com>
2025-02-15 21:26:26 +0800

    feat: impl get data from locale coin name

--- src/test/java/ht/demo/resource/ITLocaleCoinNameResource.java
+++ src/test/java/ht/demo/resource/ITLocaleCoinNameResource.java
@@ -4,6 +4,7 @@ import ht.demo.entity.Coin;
 import ht.demo.entity.LocaleCoinName;
 import ht.demo.entity.LocaleCoinNameId;
 import ht.demo.repository.CoinRepository;
+import ht.demo.repository.LocaleCoinNameRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,6 +30,8 @@ public class ITLocaleCoinNameResource {
 
     @MockitoSpyBean
     CoinRepository coinRepository;
+    @MockitoSpyBean
+    LocaleCoinNameRepository localeCoinNameRepository;
 
     private final String LocaleCoinCharName = "test-charname";
     private final String LocaleCoinLocale = "test-locale";
@@ -34,18 +40,18 @@ public class ITLocaleCoinNameResource {
     @BeforeEach
     void init() {
         Mockito.doReturn(
-            Optional.of(
-                Coin.builder()
-                    .charName(LocaleCoinCharName)
-                    .build()
+                Optional.of(
+                    Coin.builder()
+                        .charName(LocaleCoinCharName)
+                        .build()
+                )
             )
-        )
-                .when(coinRepository)
-                    .findById(Mockito.anyString());
+            .when(coinRepository)
+            .findById(Mockito.anyString());
     }
 
     @Test
-    void create() throws Exception {
+    void create() {
         var data =
             this.restTemplate
                 .postForEntity(
@@ -72,6 +78,48 @@ public class ITLocaleCoinNameResource {
         Assertions.assertEquals(
             LocaleCoinNameName,
             data.getBody().getName()
+        );
+    }
+
+    @Test
+    void read() {
+        Mockito.doReturn(
+                List.of(
+                    LocaleCoinName.builder()
+                        .id(
+                            LocaleCoinNameId.builder()
+                                .charName(LocaleCoinCharName)
+                                .locale(LocaleCoinLocale)
+                                .build()
+                        )
+                        .name(LocaleCoinNameName)
+                        .build()
+                )
+            )
+            .when(localeCoinNameRepository)
+            .findAll();
+        var data =
+            this.restTemplate
+                .exchange(
+                    "http://localhost:" + port + "/api/locale-coin-name",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<LocaleCoinName>>() {
+                    }
+                );
+        var localeCoin =
+            data.getBody().get(0);
+        Assertions.assertEquals(
+            LocaleCoinCharName,
+            localeCoin.getId().getCharName()
+        );
+        Assertions.assertEquals(
+            LocaleCoinLocale,
+            localeCoin.getId().getLocale()
+        );
+        Assertions.assertEquals(
+            LocaleCoinNameName,
+            localeCoin.getName()
         );
     }
 }
