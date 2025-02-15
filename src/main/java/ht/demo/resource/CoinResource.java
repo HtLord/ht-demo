516538e883b1287e5db02a7ac60a8f31c97c3f8c
htlord <s75757775@hotmail.com>
2025-02-15 23:50:50 +0800

    feat: impl batch update

--- src/main/java/ht/demo/resource/CoinResource.java
+++ src/main/java/ht/demo/resource/CoinResource.java
@@ -1,9 +1,6 @@
 package ht.demo.resource;
 
 import ht.demo.error.BadRequestWithErrorCodeException;
-import ht.demo.mapper.CoinMapper;
-import ht.demo.service.CoinDeskService;
-import ht.demo.service.CoinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CoinResource {
 
-    private final CoinMapper coinMapper;
-    private final CoinDeskService coinDeskService;
-    private final CoinService coinService;
 
     @GetMapping("/api/coins")
-    public void test() throws BadRequestWithErrorCodeException {
-        var data =
-            coinDeskService.fetch();
-        var entities =
-            data.getData()
-                .stream()
-                .map(x -> coinMapper.toEntity(x, x.getCharName()))
-                .toList();
-        coinService.saveOrUpdate(entities);
+    public void fetch() throws BadRequestWithErrorCodeException {
     }
 
 }
--- src/main/java/ht/demo/scheduler/CoinDeskScheduler.java
+++ src/main/java/ht/demo/scheduler/CoinDeskScheduler.java
@@ -1,12 +1,30 @@
 package ht.demo.scheduler;
 
+import ht.demo.error.BadRequestWithErrorCodeException;
+import ht.demo.mapper.CoinMapper;
+import ht.demo.service.CoinDeskService;
+import ht.demo.service.CoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CoinDeskScheduler {
 
-//    @Scheduled(fixedRate = 10000)
-//    public void pullCoinDeskAndUpdate() {
-//        throw new NotImplementedException();
-//    }
+    private final CoinMapper coinMapper;
+    private final CoinDeskService coinDeskService;
+    private final CoinService coinService;
+
+    @Scheduled(fixedRate = 10000, initialDelay = 30000)
+    public void pullCoinDeskAndUpdate() throws BadRequestWithErrorCodeException {
+        var data =
+            coinDeskService.fetch();
+        var entities =
+            data.getData()
+                .stream()
+                .map(x -> coinMapper.toEntity(x, x.getCharName()))
+                .toList();
+        coinService.saveOrUpdate(entities);
+    }
 }
