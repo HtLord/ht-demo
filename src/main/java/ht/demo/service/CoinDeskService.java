d9b2513412e7f2f600ee18fe705ae5fe325e561f
htlord <s75757775@hotmail.com>
2025-02-16 14:11:23 +0800

    feat: apply application config for coindesk endpoint

--- src/main/java/ht/demo/service/CoinDeskService.java
+++ src/main/java/ht/demo/service/CoinDeskService.java
@@ -1,20 +1,36 @@
 package ht.demo.service;
 
+import ht.demo.config.HtConfig;
 import ht.demo.dto.coin.desk.CoinDeskDtoCollection;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.net.URI;
 
 @Service
+@RequiredArgsConstructor
 public class CoinDeskService {
+
+    private final HtConfig htConfig;
 
     public CoinDeskDtoCollection fetch() {
         var defaultClient =
             RestClient.create();
         return defaultClient.get()
-                .uri(URI.create("http://localhost:3000/"))
-                .retrieve()
-                .body(CoinDeskDtoCollection.class);
+            .uri(
+                URI.create(
+                    "%s%s".formatted(
+                        htConfig.getService()
+                            .getData()
+                            .getHost(),
+                        htConfig.getService()
+                            .getData()
+                            .getEndpoint()
+                    )
+                )
+            )
+            .retrieve()
+            .body(CoinDeskDtoCollection.class);
     }
 }
