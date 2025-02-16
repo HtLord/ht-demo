e657ae59bff1985e010a907e9c2ea94d24683c04
htlord <s75757775@hotmail.com>
2025-02-16 12:00:12 +0800

    feat: impl outbound log and uuid tracing. integrate inbound and outbound to response body advice

--- src/main/java/ht/demo/config/CustomizedResponseBodyAdviceAdapter.java
+++ src/main/java/ht/demo/config/CustomizedResponseBodyAdviceAdapter.java
@@ -1,15 +1,19 @@
 package ht.demo.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @ControllerAdvice
 @Slf4j
@@ -18,7 +22,6 @@ public class CustomizedResponseBodyAdviceAdapter implements ResponseBodyAdvice {
 
     private final ObjectMapper objectMapper;
 
-
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
         return true;
@@ -26,21 +29,42 @@ public class CustomizedResponseBodyAdviceAdapter implements ResponseBodyAdvice {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        var msg =
+        var traceId =
+            UUID.randomUUID();
+        var inbound =
+            LogInfo.builder()
+                .type(LogInfoType.Inbound)
+                .httpMethod(request.getMethod())
+                .traceId(traceId)
+                .uri(request.getURI())
+                .build();
+        var outbound =
             LogInfo.builder()
                 .type(LogInfoType.Outbound)
-                .traceId("fake-trace-id")
                 .httpMethod(request.getMethod())
+                .traceId(traceId)
                 .uri(request.getURI())
                 .build();
         try {
-            var bodyString =
+            var inboundBody =
+                IOUtils.toString(
+                    request.getBody(),
+                    StandardCharsets.UTF_8
+                );
+            inbound.setBody(
+                inboundBody
+            );
+            var outboundBody =
                 objectMapper.writeValueAsString(body);
-            msg.setBody(bodyString);
-            log.info("{}", msg);
-        } catch (JsonProcessingException e) {
-            msg.setBody(e.getMessage());
-            log.info("{}", msg);
+            outbound.setBody(outboundBody);
+            log.info("{}", inbound);
+            log.info("{}", outbound);
+        } catch (IOException e) {
+            log.info(
+                "[Failure-Log-Handling]{} : {}",
+                inbound,
+                e.getMessage()
+            );
         }
         return body;
     }
--- src/main/java/ht/demo/config/LogInfo.java
+++ src/main/java/ht/demo/config/LogInfo.java
@@ -6,13 +6,14 @@ import lombok.Setter;
 import org.springframework.http.HttpMethod;
 
 import java.net.URI;
+import java.util.UUID;
 
 @Getter
 @Builder
 public class LogInfo {
 
     private LogInfoType type;
-    private String traceId;
+    private UUID traceId;
     private HttpMethod httpMethod;
     private URI uri;
     @Setter
